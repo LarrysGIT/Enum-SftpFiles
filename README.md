706818da43936164b0a100d2df0991d066675dd9
Larry Song <Larry.Song@civica.com.au>
2019-11-26 15:27:29 +1100

    new cmdlet Compare-SftpObject

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Enum-SftpFiles
-A cmdlet to enum sftp
+Cmdlets to handle local file and sftp
 
 # https://winscp.net/eng/docs/library
 Download the API DLL from above url
@@ -8,22 +8,64 @@ Import the DLL before invoke the cmdlet
 
 `Add-Type -Path WinSCPnet.dll`
 
-# Example
-Filter parameters are regular expression
-
-The following enums `File` only and `FileName` matches `2019-06-28`. e.g. `ThisIsAFile2019-06-28.txt`
-
+# Examples
+### Import commands
 ```powershell
-
 Add-Type -Path WinSCPnet.dll
 . .\SftpCommands.ps1
-
-Enum-SftpFiles -HostName $SFTPServer -Username $SFTPUser -Password $SFTPPass -RemotePath "/" -AcceptAllCertificate -Recurse -FileNameIncludeFilter "2019-06-28" -Directory:$false
-
-Download-SftpObject -HostName $SFTPServer -Username $SFTPUser -Password $SFTPPass -RemotePath "/somepath/test" -LocalPath "C:\Temp" -AcceptAllCertificate
-
-Upload-SftpObject -HostName $SFTPServer -Username $SFTPUser -Password $SFTPPass -LocalPath "C:\Temp" -RemotePath "/somepath/test" -AcceptAllCertificate
-
 ```
 
+### Initial a new SFTP session
+```powershell
+$session = New-SftpSession -HostName sftp.dns.name -Username sftpuser -Password sftppassword -AcceptAllCertificate
+```
 
+* Create a dedicated SFTP session is suggested, although, pass username and password works too. However, initial the SFTP session is time costly. Script performance will be dragged if too many sessions open / close.
+
+* Don't forget to close SFTP session.
+
+```powershell
+Close-SftpSession -Session $session
+```
+
+### List SFTP objects
+The following enums `File` only and `FileName` matches `2019-06-28`. e.g. `ThisIsAFile2019-06-28.txt`
+
+`Filter parameters` are regular expression
+```powershell
+Enum-SftpFiles -HostName $SFTPServer -Username $SFTPUser -Password $SFTPPass -RemotePath "/" -AcceptAllCertificate -Recurse -FileNameIncludeFilter "2019-06-28" -Directory:$false
+```
+
+### "Get/Download" objects from SFTP
+```powershell
+Download-SftpObject -Session $session -RemotePath "/somepath/test" -LocalPath "C:\Temp"
+```
+
+### "Put/Upload" objects to SFTP
+```powershell
+Upload-SftpObject -Session $session -LocalPath "C:\Temp" -RemotePath "/somepath/test"
+```
+
+### da da! Here is a new cmdlet recently wrote to support new requirement
+```powershell
+Compare-SftpObject -Session $session -LocalPath .\ -RemotePath "/remotepath" -Recurse -CompareSize -CompareLastWriteTime
+```
+
+* This new cmdlet allows compare contents in a local path and a remote SFTP path, by checking the path existence, size (file) and last modified time (file).
+
+* Q: Why not compare files by hash? A: Need to download the data from SFTP and get the hash, time costly. For now, simply not supported.
+
+* Q: What's the purpose of this cmdlet? A: Check following scenario.
+
+```txt
+~ Rachael: Hey! Larry! Can you send a folder in the share to SFTP, so the team in XXX can pick it up? (XXX team are not able to access our share, just business)
+~ Me: Sure!
+    Upload-SftpObject is the one, makes life easy for everyone (problem solved for now)
+
+<<<2 months passed>>>
+
+~ Rachael: Hey! Larry! The thing you setup 2 months ago works fine, but my team has found it getting slower and slower, can you take a look please?
+~ Me: Sure!
+    In past 2 months, the local share has been populated with a large amount of data, the Upload-SftpObject cmd copies everything, including files not changed.
+    Compare-SftpObject is the one, I use the cmdlet to get files have different size, path or last modified time. Selectively use Upload-SftpObject to put changed data to SFTP.
+```
--- SftpCommands.ps1
+++ SftpCommands.ps1
@@ -151,9 +151,6 @@ function Download-SftpObject()
         $Session = New-Object WinSCP.Session
         $Session.Open($sessionOptions)
     }
-    # $RemotePath = $RemotePath.TrimEnd("/")
-    # $LocalPath = $LocalPath.TrimEnd("\")
-    # $LocalPath = "${LocalPath}\"
     $r = $Session.GetFiles($RemotePath, $LocalPath, $false)
     if($CloseSession)
     {
@@ -196,9 +193,6 @@ function Upload-SftpObject()
         $Session = New-Object WinSCP.Session
         $Session.Open($sessionOptions)
     }
-    # $LocalPath = $LocalPath.TrimEnd("\")
-    # $RemotePath = $RemotePath.TrimEnd("/")
-    # $RemotePath = "${RemotePath}/"
     $r = $Session.PutFiles($LocalPath, $RemotePath, $false)
     if($CloseSession)
     {
@@ -206,4 +200,287 @@ function Upload-SftpObject()
         try{$session.Dispose()}catch{}
     }
     return $r
+}
+
+function New-SftpSession()
+{
+    PARAM(
+        [string]$HostName,
+        [string]$Username,
+        [string]$Password,
+        [switch]$AcceptAllCertificate
+    )
+    $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+        Protocol = [WinSCP.Protocol]::Sftp
+        HostName = $HostName
+        UserName = $Username
+        Password = $Password
+        GiveUpSecurityAndAcceptAnySshHostKey = $AcceptAllCertificate
+    }
+    $Session = New-Object WinSCP.Session
+    $Session.Open($sessionOptions)
+    return $Session
+}
+
+function Close-SftpSession()
+{
+    PARAM(
+        [WinSCP.Session]$Session
+    )
+    try{$session.Close()}catch{}
+    try{$session.Dispose()}catch{}
+}
+
+function Compare-SftpObject()
+{
+    PARAM(
+        [WinSCP.Session]$Session,
+        [string]$HostName,
+        [string]$Username,
+        [string]$Password,
+        [string]$LocalPath,
+        [string]$RemotePath,
+        [switch]$Recurse,
+        [switch]$CompareSize = $true,
+        [switch]$CompareLastWriteTime,
+        [switch]$File = $true,
+        [switch]$Directory = $true,
+        [switch]$AcceptAllCertificate
+    )
+    $LocalPath = [io.path]::GetFullPath($LocalPath) # in case relative path like "."
+    $LocalPath = $LocalPath -ireplace "\\$", "\"
+    $RemotePath = $RemotePath -ireplace "/$", "/"
+    if(!$Session)
+    {
+        if(!$HostName -or !$Username -or !$Password)
+        {
+            throw "Please provide at least one of [Session] and ([HostName],[Username],[Password])"
+            return
+        }
+    }
+    $CloseSession = $false
+    if(!$Session)
+    {
+        $CloseSession = $true
+        $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+            Protocol = [WinSCP.Protocol]::Sftp
+            HostName = $HostName
+            UserName = $Username
+            Password = $Password
+            GiveUpSecurityAndAcceptAnySshHostKey = $AcceptAllCertificate
+        }
+        $Session = New-Object WinSCP.Session
+        $Session.Open($sessionOptions)
+    }
+    if($File -and $Directory)
+    {
+        $localObjects = Get-ChildItem -Path $LocalPath -Recurse:$Recurse -Force
+    }
+    elseif($File -and !$Directory)
+    {
+        $localObjects = Get-ChildItem -Path $LocalPath -File -Recurse:$Recurse -Force
+    }
+    elseif(!$File -and $Directory)
+    {
+        $localObjects = Get-ChildItem -Path $LocalPath -Directory -Recurse:$Recurse -Force
+    }
+    else
+    {
+        $localObjects = @()
+    }
+    $remoteObjects = Enum-SftpFiles -Session $Session -RemotePath $RemotePath -Recurse:$Recurse -File:$File -Directory:$Directory
+    class ComparedObject
+    {
+        [object]$LocalObject = $null
+        [object]$RemoteObject = $null
+        [string[]]$Result = @()
+    }
+    # Direction: based on local objects, check remote objects
+    $CheckedRemoteObjects = @()
+    foreach($localobject in $localObjects)
+    {
+        $ComparedObject = New-Object ComparedObject
+        $ComparedObject.LocalObject = $localobject
+        $matched = $false
+        $localobjectFullName_Relative = $localobject.FullName.Remove(0, $LocalPath.Length).TrimStart("\")
+        foreach($remoteobject in $remoteObjects)
+        {
+            $remoteobjectFullName_Relative = $remoteobject.FullName.Remove(0, $RemotePath.Length).TrimStart("/").Replace("/", "\")
+            if($localobjectFullName_Relative -eq $remoteobjectFullName_Relative)
+            {
+                # local and remote objects have the same relative path
+                $matched = $true
+                break
+            }
+        }
+        if($matched)
+        {
+            $CheckedRemoteObjects += $remoteobject.FullName
+            $ComparedObject.RemoteObject = $remoteobject
+            if($ComparedObject.LocalObject.Mode -match "^d")
+            {
+                # Local object is a directory
+                if($ComparedObject.RemoteObject.IsDirectory)
+                {
+                    # both are directory
+                    $ComparedObject.Result += "DirectoryBoth"
+                }
+                else
+                {
+                    # remote is a file
+                    $ComparedObject.Result += "DirectoryLocal,FileRemote"
+                }
+            }
+            else
+            {
+                # Local object is a file
+                if($ComparedObject.RemoteObject.IsDirectory)
+                {
+                    # remote is a directory
+                    $ComparedObject.Result = "FileLocal,DirectoryRemote"
+                }
+                else
+                {
+                    # both are file
+                    $ComparedObject.Result += "FileBoth"
+                    if($CompareSize)
+                    {
+                        if($ComparedObject.LocalObject.Length -eq $ComparedObject.RemoteObject.Length)
+                        {
+                            # same file length
+                            $ComparedObject.Result += "SameLength"
+                        }
+                        else
+                        {
+                            # different file length
+                            $ComparedObject.Result += "DifferentLength"
+                        }
+                    }
+                    if($CompareLastWriteTime)
+                    {
+                        if($ComparedObject.LocalObject.LastWriteTime -eq $ComparedObject.RemoteObject.LastWriteTime)
+                        {
+                            # same LastWriteTime
+                            $ComparedObject.Result += "SameLastWriteTime"
+                        }
+                        else
+                        {
+                            $ComparedObject.Result += "DifferentLastWriteTime"
+                            if($ComparedObject.LocalObject.LastWriteTime -ge $ComparedObject.RemoteObject.LastWriteTime)
+                            {
+                                $ComparedObject.Result += "LocalNewer"
+                            }
+                            else
+                            {
+                                $ComparedObject.Result += "RemoteNewer"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            $ComparedObject.Result += "LocalOnly"
+        }
+        $ComparedObject
+    }
+    # Direction: based on remote objects, check local objects
+    foreach($remoteobject in $RemoteObjects)
+    {
+        if($CheckedRemoteObjects -contains $remoteobject.FullName)
+        {
+            # checked already, no need to do again
+            Continue
+        }
+        $ComparedObject = New-Object ComparedObject
+        $ComparedObject.RemoteObject = $remoteobject
+        $matched = $false
+        $remoteobjectFullName_Relative = $remoteobject.FullName.Remove(0, $RemotePath.Length).TrimStart("/")
+        foreach($localobject in $LocalObjects)
+        {
+            $localobjectFullName_Relative = $localobject.FullName.Remove(0, $LocalPath.Length).TrimStart("\").Replace("\", "/")
+            if($localobjectFullName_Relative -eq $remoteobjectFullName_Relative)
+            {
+                # local and remote objects have the same relative path
+                $matched = $true
+                break
+            }
+        }
+        if($matched)
+        {
+            $ComparedObject.LocalObject = $localobject
+            if($ComparedObject.LocalObject.Mode -match "^d")
+            {
+                # Local object is a directory
+                if($ComparedObject.RemoteObject.IsDirectory)
+                {
+                    # both are directory
+                    $ComparedObject.Result += "DirectoryBoth"
+                }
+                else
+                {
+                    # remote is a file
+                    $ComparedObject.Result += "DirectoryLocal,FileRemote"
+                }
+            }
+            else
+            {
+                # Local object is a file
+                if($ComparedObject.RemoteObject.IsDirectory)
+                {
+                    # remote is a directory
+                    $ComparedObject.Result = "FileLocal,DirectoryRemote"
+                }
+                else
+                {
+                    # both are file
+                    $ComparedObject.Result += "FileBoth"
+                    if($CompareSize)
+                    {
+                        if($ComparedObject.LocalObject.Length -eq $ComparedObject.RemoteObject.Length)
+                        {
+                            # same file length
+                            $ComparedObject.Result += "SameLength"
+                        }
+                        else
+                        {
+                            # different file length
+                            $ComparedObject.Result += "DifferentLength"
+                        }
+                    }
+                    if($CompareLastWriteTime)
+                    {
+                        if($ComparedObject.LocalObject.LastWriteTime -eq $ComparedObject.RemoteObject.LastWriteTime)
+                        {
+                            # same LastWriteTime
+                            $ComparedObject.Result += "SameLastWriteTime"
+                        }
+                        else
+                        {
+                            $ComparedObject.Result += "DifferentLastWriteTime"
+                            if($ComparedObject.LocalObject.LastWriteTime -ge $ComparedObject.RemoteObject.LastWriteTime)
+                            {
+                                $ComparedObject.Result += "LocalNewer"
+                            }
+                            else
+                            {
+                                $ComparedObject.Result += "RemoteNewer"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            $ComparedObject.Result += "RemoteOnly"
+        }
+        $ComparedObject
+    }
+    if($CloseSession)
+    {
+        try{$session.Close()}catch{}
+        try{$session.Dispose()}catch{}
+    }
 }
